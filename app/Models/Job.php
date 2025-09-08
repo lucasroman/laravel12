74469311aac63f18c420dd859ce4193b662c996f
Lucas Roman <lucasroman@gmail.com>
2025-09-08 00:36:45 -0300

    Update Job model to use data from database
    - Remove hardcode data
    - Set specific table name (not use conventions)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,35 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-                'description' => "In charge of the company's productivity.",
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-                'description' => "Is the indispensable workfoce.",
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-                'description' => "Transmits the knoledges to new generations.",
-            ]
-        ];
-    }
-
-    // Return the first array found with that id parameter
-    public static function find(int $id): array 
-    {
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id) ?? abort(404);
-    }
+    // Match with 'job_listing' table instead of 'jobs' table
+    protected $table = 'job_listing';
 }
