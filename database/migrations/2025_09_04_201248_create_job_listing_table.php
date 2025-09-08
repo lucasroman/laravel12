b4abfa96a98486b624d8e705543ea68a992d6c81
Lucas Roman <lucasroman@gmail.com>
2025-09-08 00:39:01 -0300

    Add column 'description' to job_listing table

--- database/migrations/2025_09_04_201248_create_job_listing_table.php
+++ database/migrations/2025_09_04_201248_create_job_listing_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('salary');
+            $table->string('description');
             $table->timestamps();
         });
     }
