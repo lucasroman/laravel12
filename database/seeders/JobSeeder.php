1654b31596f8a0185249fe3f5916f53182779adf
Lucas Roman <lucasroman@gmail.com>
2025-09-11 00:08:30 -0300

    JobSeeder: fill timestamps
    Timestapms fields now aren't null.

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Job;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 // Empty table before seed it
 Job::truncate();
@@ -21,19 +22,25 @@ class JobSeeder extends Seeder
         DB::table('job_listing')->insert([
             'title' => 'Director',
             'salary' => '$40,000',
-            'description' => 'On charge of take important desicions.'
+            'description' => 'On charge of take important desicions.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         DB::table('job_listing')->insert([
             'title' => 'Programmer',
             'salary' => '$10,000',
-            'description' => 'Develop all users applications.'
+            'description' => 'Develop all users applications.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         DB::table('job_listing')->insert([
             'title' => 'Teacher',
             'salary' => '$30,000',
-            'description' => 'Transmit knowledges to others.'
+            'description' => 'Transmit knowledges to others.',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
     }
 }
