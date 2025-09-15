46fd68cadd00ff25250956d5492b4a6695141a30
Lucas Roman <lucasroman@gmail.com>
2025-09-14 22:48:03 -0300

    Job view: add edit icon

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -2,10 +2,13 @@
     <x-slot:heading>    
         <a href="/jobs" class="hover:underline">{{ $jobSelected['title'] }}'s Job</a>
     </x-slot:heading>
-
-    <div><b>{{ $jobSelected['title'] }}</b></div>
-
+    
+    <div class="flex items-center space-x-2">
+        <b>{{ $jobSelected['title'] }}</b> 
+        <img src="{{ asset('storage/icons/edit.png') }}" alt="edit"> 
+    </div>
     <p>This job pays <b>{{ $jobSelected['salary'] }}</b> per year.</p>
 
     <div>{{ $jobSelected['description'] ?? 'No description setted.'}}</div>
+
 </x-layout>
