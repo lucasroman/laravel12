d28c8884ea70f9b2650c31e8189d8f643603433b
Lucas Roman <lucasroman@gmail.com>
2025-09-16 23:08:40 -0300

    Update Job model with factory
    Add neccesary library for generate factories.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory;
+    
     // Match with 'job_listing' table instead of 'jobs' table
     protected $table = 'job_listing';
 
