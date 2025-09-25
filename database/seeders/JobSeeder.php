6666101f27c21dda134619d74b62f0b74b557502
Lucas Roman <lucasroman@gmail.com>
2025-09-25 02:13:17 -0300

    Update JobSeeder
    - Move truncate table inside run function to
    avoid errors.
    - Add Schema library for disable/enable foreign
    ids restrictions.
    - Create three default employers and add its ids
    to default Jobs.

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -3,25 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\Job;
+use App\Models\Employer;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-// Empty table before seed it
-Job::truncate();
 
 class JobSeeder extends Seeder
 {
-
+    
     /**
      * Run the database seeds.
-     */
+    */
     public function run(): void
     {
+        // Empty table before seed it
+        Schema::disableForeignKeyConstraints();
+        Job::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $employers = Employer::factory(3)->create();
+
         Job::create([
             'title' => 'Director',
             'salary' => '$40,000',
-            'employer_id' => 1,
+            'employer_id' => $employers[0]->id,
             'description' => 'On charge of take important desicions.',
         ]);
 
@@ -29,14 +36,14 @@ class JobSeeder extends Seeder
         Job::create([
             'title' => 'Programmer',
             'salary' => '$10,000',
-            'employer_id' => 1,
+            'employer_id' => $employers[1]->id,
             'description' => 'Develop all users applications.',
         ]);
 
         Job::create([
             'title' => 'Teacher',
             'salary' => '$30,000',
-            'employer_id' => 1,
+            'employer_id' => $employers[2]->id,
             'description' => 'Transmit knowledges to others.',
         ]);
     }
