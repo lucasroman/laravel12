b5d3d59aad3c730bc6fd3ec89209f7d0d16fe568
Lucas Roman <lucasroman@gmail.com>
2026-02-10 09:26:33 -0300

    Job listing: stylize list

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,18 @@
         Jobs Listing
     </x-slot:heading>
 
-    <ul class="list-none">
+    <div class="list-none space-y-4 ">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-slate-400 hover:underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays Salary: {{ $job['salary'] }} per year.
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border 
+                border-gray-200 rounded-lg bg-gray-900">
+                    <div class="font-bold text-blue-500 text-sm">
+                        {{ $job->employer->name }}
+                    </div>
+                    <div>
+                        <strong>{{ $job['title'] }}:</strong> Pays Salary: 
+                            {{ $job['salary'] }} per year.
+                    </div>
                 </a>
-            </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
