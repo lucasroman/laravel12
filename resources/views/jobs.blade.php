d2eb8359fa2fd1cf20188efbf1eceabd750a5b96
Lucas Roman <lucasroman@gmail.com>
2026-02-21 23:18:58 -0300

    Add pagination to Jobs listing

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -16,5 +16,9 @@
                     </div>
                 </a>
         @endforeach
+
+        <div>
+            {{ $jobs->links() }}    
+        </div>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('/', 'home');
 Route::get('/jobs', function() {
     /* Not use "Job::all()" because require more queries, one query more by 
     each employer, insted use Eager Loading as shown below */
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->paginate(3);
     
     return view('jobs', [
         'jobs' => $jobs
