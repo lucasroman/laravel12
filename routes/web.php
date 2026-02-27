52b21703e7e2f6c8548257dbfc88f142fd8fbc08
Lucas Roman <lucasroman@gmail.com>
2026-02-26 23:13:44 -0300

    Change pagination jobs to simplePaginate

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('/', 'home');
 Route::get('/jobs', function() {
     /* Not use "Job::all()" because require more queries, one query more by 
     each employer, insted use Eager Loading as shown below */
-    $jobs = Job::with('employer')->paginate(3);
+    $jobs = Job::with('employer')->simplePaginate(2);
     
     return view('jobs', [
         'jobs' => $jobs
