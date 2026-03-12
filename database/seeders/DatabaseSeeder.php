2a151f2460cff84401bd1febe10d0c672d790192
Lucas Roman <lucasroman@gmail.com>
2026-03-11 23:34:26 -0300

    DatabaseSeeder: add user default record

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
             'email' => 'test@example.com',
         ]);
         
