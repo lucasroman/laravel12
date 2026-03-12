d4e85e8b83ad125fd4a62ae7cec735c7ed018226
Lucas Roman <lucasroman@gmail.com>
2026-03-11 23:37:41 -0300

    Web: change quantity of records to see

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('/', 'home');
 Route::get('/jobs', function() {
     /* Not use "Job::all()" because require more queries, one query more by 
     each employer, insted use Eager Loading as shown below */
-    $jobs = Job::with('employer')->simplePaginate(2);
+    $jobs = Job::with('employer')->simplePaginate(5);
     
     return view('jobs', [
         'jobs' => $jobs
