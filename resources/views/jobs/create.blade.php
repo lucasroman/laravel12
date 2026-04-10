9484ed44a208cc2b94646b429db81175e31695df
Lucas Roman <lucasroman@gmail.com>
2026-04-09 23:25:17 -0300

    Update create job view
    - Add Tailwind form component.
    - Remove unnesessary sections from original form.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,7 +3,40 @@
         Create Job
     </x-slot:heading>
 
-    <p>TODO</p>
-</x-layout>
+<form method="POST" action="">
+  <div class="space-y-12">
+    <div class="border-b border-gray-900/10 pb-12 dark:border-white/10">
+      <h2 class="text-base/7 font-semibold text-gray-900 dark:text-white">Create a New Job</h2>
+      <p class="mt-1 text-sm/6 text-gray-600 dark:text-gray-400">We just need a handful of details from you.</p>
 
-Create Job
+    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-4">
+          <label for="title" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Title</label>
+          <div class="mt-2">
+            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
+              <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-transparent dark:text-white dark:placeholder:texTitle-500" />
+            </div>
+          </div>
+        </div>
+        </div>
+
+      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="sm:col-span-4">
+          <label for="salary" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Salary</label>
+          <div class="mt-2">
+            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 dark:bg-white/5 dark:outline-white/10 dark:focus-within:outline-indigo-500">
+              <input id="salary" type="text" name="salary" placeholder="$50,000 Per year" class="block min-w-0 grow bg-white py-1.5 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500" />
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+  </div>
+
+  <div class="mt-6 flex items-center justify-end gap-x-6">
+    <button type="button" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Cancel</button>
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:focus-visible:outline-indigo-500">Save</button>
+  </div>
+</form>
+
+</x-layout>
